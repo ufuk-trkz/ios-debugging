67a01c4bfd54fff3e9b987e329f9b3c5e626e2d9
Michael Redig <mredig@gmail.com>
2019-09-09 20:56:38 -0500

    changed warning to error for firebase

--- JournalCoreData/Model Controllers/EntryController.swift
+++ JournalCoreData/Model Controllers/EntryController.swift
@@ -9,7 +9,7 @@
 import Foundation
 import CoreData
 
-#warning("Change this value to your own firebase database!")
+#error("Change this value to your own firebase database! (and then delete this line)")
 let baseURL = URL(string: "https://journal-syncing.firebaseio.com/")!
 
 class EntryController {
