a8e97a3f73541fe4cf451ac019c2518d0d45714a
Ufuk Türközü <ufuk_trkz@hotmail.de>
2020-03-16 21:38:20 +0100

    Added new firebase URL

--- JournalCoreData/Model Controllers/EntryController.swift
+++ JournalCoreData/Model Controllers/EntryController.swift
@@ -9,8 +9,7 @@
 import Foundation
 import CoreData
 
-#error("Change this value to your own firebase database! (and then delete this line)")
-let baseURL = URL(string: "https://journal-syncing.firebaseio.com/")!
+let baseURL = URL(string: "https://journalios14.firebaseio.com/")!
 
 class EntryController {
     
